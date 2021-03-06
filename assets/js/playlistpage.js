1e5ab02a5ca5db24689b6b8c2d6ef64fcfb47ddd
Rayna W <raynagweinreb@gmail.com>
2021-03-06 09:55:17 -0800

    small changes

---
+++ assets/js/playlistpage.js
@@ -0,0 +1,11 @@
+//var buttonClick = function(event){
+   // var mood = event.target.GetAttribute()
+//}
+//spotifyUrl= "https://api.spotify.com/v1/search?q=happy&type=playlist&market=US&limit=10&offset=5-Content-Type: application/jsonHAuthorization: Bearer BQChcoEpHotR9GMWvlKJOK76yjtdWMuJW6Q3L5Bn-sZeaL1llYvQAHEzP1YD0oPZbo_W0utpHDqF2auBBdEPOBZxtOx4mFbleJBrqeZquxPGqd3U6hz_7lvpN1EbVqKhVMrBgP5NEmTYIs_sIi7Q"
+ //   fetch(spotifyUrl)
+  //  .then(function (response) {
+   //   if (response.ok) {
+   //     response.json().then(function (data) {
+   //         console.log(data);
+//})
+     // }})
