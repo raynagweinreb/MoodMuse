567375a85d87db8e5544c0815b0d84c2982f32da
Antonio Barrera <Barrera.antonio@gmail.com>
2021-03-05 22:37:54 -0800

    this alone should fix it

--- assets/script.js
+++ assets/script.js
@@ -29,6 +29,9 @@ else{
         },
         error: function (jqXHR, status, err) {
             alert("Local error callback.");
+            var spotifyTokenLabel = 'spotifyToken';
+            localStorage.removeItem(spotifyTokenLabel)
+            document.location = 'https://accounts.spotify.com/authorize?client_id=8500729d9a47478fb0bd212d4e8e309e&response_type=token&redirect_uri=https://raynagweinreb.github.io/MoodMuse/test.html';
           }        
         })
 var getUserId = function () {
