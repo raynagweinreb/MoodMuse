cb540226a9ee70bcc0f72b1a2b095dddc35c8249
Antonio Barrera <Barrera.antonio@gmail.com>
2021-03-05 22:53:04 -0800

    w

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@
 //this gets the user mood 
 buttonVal = $('#button-container')
 
-buttonVal.on('click','button',function(event){
+buttonVal.on('click', 'button', function (event) {
     event.preventDefault();
     var mood = $(this).text();
     console.log(mood)
@@ -13,37 +13,38 @@ buttonVal.on('click','button',function(event){
 
 var spotifyTokenLabel = 'spotifyToken'
 var spotifyToken = localStorage.getItem(spotifyTokenLabel)
-if(!spotifyToken){
+if (!spotifyToken) {
     //redirect to spotify login
     document.location = 'https://accounts.spotify.com/authorize?client_id=8500729d9a47478fb0bd212d4e8e309e&response_type=token&redirect_uri=https://raynagweinreb.github.io/MoodMuse/test.html';
 }
-else{
+else {
     //move to second page
     $.ajax({
         url: 'https://api.spotify.com/v1/me',
         headers: {
             'Authorization': 'Bearer ' + spotifyToken
         },
-        success: function(response) {
+        success: function (response) {
             //show welcome msg...
+            alert('hiiiiiiii<3')
         },
         error: function (jqXHR, status, err) {
-            alert("Local error callback.");
             var spotifyTokenLabel = 'spotifyToken';
             localStorage.removeItem(spotifyTokenLabel)
             document.location = 'https://accounts.spotify.com/authorize?client_id=8500729d9a47478fb0bd212d4e8e309e&response_type=token&redirect_uri=https://raynagweinreb.github.io/MoodMuse/test.html';
-          }        
-        })};
+        }
+    })
+}
 var getUserId = function () {
     var queryString = document.location.search;
     var userId = queryString.split('=')[0];
     console.log(userId)
-  
+
     if (userId) {
-      userIdEl.textContent = userId;
-  
-      document.location.replace('./index.html');
+        userIdEl.textContent = userId;
+
+        document.location.replace('./index.html');
     } else {
-      document.location.replace('./index.html');
+        document.location.replace('./index.html');
     }
-  };
+};
