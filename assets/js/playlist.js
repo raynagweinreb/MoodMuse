63426e58897443c735db4fddab681bb778552673
Rayna W <raynagweinreb@gmail.com>
2021-03-10 15:27:30 -0800

    add js to show list of playlists

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -1,14 +1,57 @@
 var playlistheader = document.getElementById("playlistHeader")
 mood = localStorage.getItem("mood")
+console.log(mood)
 playlistHeader.append(mood)
+var html=[]
 
 
-function getPlaylists(mood){
+
+function getPlaylists(event){
     fetch("https://openwhyd.org/search?q="+mood+"&format=json")
-    .then(function (data) {
-      if (data.ok) {
-        data.json().then(function (data) {
-           console.log(data)
- })
-      }})}
+    .then(function (playlist) {
+      if (playlist.ok) {
+        playlist.json().then(function (playlist) {
+           console.log(playlist);
+         
+           for ( var i = 0; i <=10; i++ ) {
+          var playlistTitle= playlist.results.playlists[i].name
+         
+          var playlistImg=playlist.results.playlists[i].img
+          img= "https://openwhyd.org"+playlistImg
+          console.log(playlistUrl)
+          var playlistUrl=playlist.results.playlists[i].url
+          var url="https://openwhyd.org"+playlistUrl
+           
+   
+
+
+          html.push(' <div class="media">')
+          html.push('<div id="playlistImg" class="media-left">')
+          html.push(' <figure class="image is-96x96">')
+          html.push('<img src='+img+'alt="Placeholder image">')
+          html.push("</figure>")
+          html.push("</div>")
+          html.push('<div class="media-content">')
+          html.push('<p id= "playlistTitle" class="title is-4">'+playlistTitle+'</p>')
+          html.push('<footer class="card-footer">')
+          html.push('<a class="card-footer-item" href='+url+'>View Playlist</a>')
+          html.push('<a href="eventpage.html" class="card-footer-item" id="events">See Events</a>')
+          html.push('</div>')
+          html.push('</div>')
+        $("#list").html(html.join(''));
+          } 
+    
+
+
+
+
+
+           
+        })}})}
+       
+
+    
+    
+
+   
       window.addEventListener("load",getPlaylists)
