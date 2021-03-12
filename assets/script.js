f0c4526a126a655d4686683af0a7b4232c4c86af
Rayna W <raynagweinreb@gmail.com>
2021-03-11 18:56:11 -0800

    add styling

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 var getHotTracks = function(){
     var topAPI= 'https://openwhyd.org/search?q=happy&format=json';
     
     fetch(topAPI)
     .then(function (response){
         return response.json();
-=======
 //When they click mood, we check for token, if good, send them to next page
 //else redirect to get token
 
@@ -17,11 +15,10 @@ buttonVal.on('click', 'button', function (event) {
     event.preventDefault();
     var mood = $(this).text();
     console.log(mood);
->>>>>>> c3d2ab729903003cceb0b284d8763cefa6179853
 })
     .then(function(data){
         console.log(data)
     })
 
-};
+})};
 getHotTracks();
